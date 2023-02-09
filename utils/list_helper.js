6293a45774cef57d42f87456453ff52f99ef8b5d
saurabh chauhan <sourabh1202@gmail.com>
2023-02-10 01:41:16 +0200

    ex-4.4-done

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  const likes = blogs.reduce((count, blog) => {
+    return count + blog.likes;
+  }, 0);
+
+  return likes;
+};
+
 module.exports = {
   dummy,
+  totalLikes
 };
